da7d146e38e3d8d0c50e610f0abcb7fd0aef4985
ubuntu <rajasprakruth@gmail.com>
2016-09-08 19:23:00 +0530

    a

--- fib/fib/urls.py
+++ fib/fib/urls.py
@@ -13,13 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url,include,patterns
 from django.contrib import admin
 from django.contrib.auth import views
+from django.conf import settings
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('fibannocci.urls')),
-    'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
+
+
+
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
