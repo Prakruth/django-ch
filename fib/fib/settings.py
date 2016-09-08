e6d1a96cc984a760b69f0db4a1f0534969fa9d82
ubuntu <rajasprakruth@gmail.com>
2016-09-08 09:03:34 +0530

    asdad

--- fib/fib/settings.py
+++ fib/fib/settings.py
@@ -80,12 +80,7 @@ WSGI_APPLICATION = 'fib.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
